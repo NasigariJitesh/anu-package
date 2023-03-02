1170e6c457e65f8b5c4130926ae329ff533d272e
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2023-03-02 12:51:03 +0530

    clean up

--- apps/docs/pages/components/checkbox.tsx
+++ apps/docs/pages/components/checkbox.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import CheckboxDoc from 'screens/components/checkbox';
+
 const Checkbox = () => {
   return <CheckboxDoc />;
 };
--- apps/docs/screens/components/checkbox.tsx
+++ apps/docs/screens/components/checkbox.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { checkBoxDocumentation } from 'services/docs/checkbox';
 import Content from 'src/sections/content';
 
