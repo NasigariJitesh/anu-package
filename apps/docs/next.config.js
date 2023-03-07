87d6536b537f3bc5c2161853ccc6a2ed41385768
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2023-03-07 19:10:31 +0530

    cleaned up accordions in sidebar

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -21,8 +21,6 @@ const nextConfig = withExpo({
     appDir: false,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   async redirects() {
