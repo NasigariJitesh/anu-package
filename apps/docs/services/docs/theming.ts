ba6c4265c232c330db45f6e87e9956ffb21baa38
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2023-03-25 10:22:58 +0530

    added theming page

---
+++ apps/docs/services/docs/theming.ts
@@ -0,0 +1,39 @@
+import { HeadingProps } from 'components/right-sidebar/right-sidebar';
+
+export const themingIndex: HeadingProps = {
+  heading: 'theming-index:mainHeading',
+  links: [
+    {
+      title: 'theming-index:title-1',
+      link: '#create-theme',
+    },
+    {
+      title: 'theming-index:title-2',
+      link: '#usage',
+    },
+    {
+      title: 'theming-index:title-3',
+      link: '#color-modes',
+    },
+    {
+      title: 'theming-index:title-4',
+      link: '#default-color-modes',
+    },
+    {
+      title: 'theming-index:title-5',
+      link: '#light-theme',
+    },
+    {
+      title: 'theming-index:title-6',
+      link: '#dark-theme',
+    },
+    {
+      title: 'theming-index:title-7',
+      link: '#custom-fonts',
+    },
+    {
+      title: 'theming-index:title-8',
+      link: '#additional-links',
+    },
+  ],
+};
