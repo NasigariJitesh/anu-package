6d518e93d9249398e5d2edc30113f2dc0fd07c8a
jitesh <jitesh.nassigari@mocktheta.co>
2023-04-28 13:00:30 +0530

    updated default values

--- packages/anu/lib/advanced/date-picker/components/calendar/default.ts
+++ packages/anu/lib/advanced/date-picker/components/calendar/default.ts
@@ -1,7 +1,6 @@
 import { CalendarEditProps } from '../../types';
 
 export const defaultCalendarEditProps: Partial<CalendarEditProps> = {
-  label: '',
   startLabel: 'Start',
   endLabel: 'End',
 };
