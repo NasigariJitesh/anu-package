15c67c8d7a8d3415440b321abec9b65adfd5216e
NasigariJitesh <72294952+NasigariJitesh@users.noreply.github.com>
2023-06-09 17:19:55 +0530

    added margin for activity indicator

--- packages/anu/lib/primitives/button/components/regular/common.tsx
+++ packages/anu/lib/primitives/button/components/regular/common.tsx
@@ -1,16 +1,9 @@
-import {
-	generateHoverStyles,
-	getCombinedStylesForText,
-} from 'anu/common/utils';
+import { generateHoverStyles, getCombinedStylesForText } from 'anu/common/utils';
 import { useTheme } from 'anu/config';
 import { Container, Icon, TouchableRipple } from 'anu/lib';
 import Typography from 'anu/lib/primitives/typography';
 import { useSx } from 'dripsy';
-import {  
-	ActivityIndicator,
-	GestureResponderEvent,
-	PressableStateCallbackType,
-} from 'react-native';
+import { ActivityIndicator, GestureResponderEvent, PressableStateCallbackType } from 'react-native';
 
 import { RegularButtonProps as ButtonProps } from '../../types';
 import { getButtonStyles, getLabelStyles } from '../../utils';
@@ -21,68 +14,70 @@ import { getButtonStyles, getLabelStyles } from '../../utils';
  * @param {ButtonProps} props - all the props related to the component
  */
 export const RenderComponent = (props: ButtonProps) => {
-	const theme = useTheme();
+  const theme = useTheme();
 
-	const { styles, stateLayerStyles } = getButtonStyles(props, theme);
-	const labelStyles = {
-		...getLabelStyles(props),
-		color: styles.color as string,
-	};
+  const { styles, stateLayerStyles } = getButtonStyles(props, theme);
+  const labelStyles = {
+    ...getLabelStyles(props),
+    color: styles.color as string,
+  };
 
-	const generateStyles = (state: PressableStateCallbackType) => {
-		return generateHoverStyles(state, stateLayerStyles, useSx);
-	};
+  const activityIndicatorStyle = {
+    marginHorizontal: 2,
+  };
 
-	const getIcon = () => {
-		const { icon } = props;
+  const generateStyles = (state: PressableStateCallbackType) => {
+    return generateHoverStyles(state, stateLayerStyles, useSx);
+  };
 
-		if (icon)
-			return 'name' in icon ? (
-				<Icon
-					size={18}
-					name={icon.name as never}
-					{...icon.props}
+  const getIcon = () => {
+    const { icon } = props;
+
+    if (icon)
+      return 'name' in icon ? (
+        <Icon
+          size={18}
+          name={icon.name as never}
+          {...icon.props}
           //@ts-expect-error reason: the style type will always be text style only
-					style={getCombinedStylesForText(
-						{ color: styles.color },
-						icon.props?.style,
-					)}
-				/>
-			) : (
-				icon
-			);
-		else return null;
-	};
+          style={getCombinedStylesForText({ color: styles.color as string }, icon.props?.style)}
+        />
+      ) : (
+        icon
+      );
+    else return null;
+  };
 
-	return (
-		<Container
-			dataSet={props.dataSets?.containerDataSet}
-			disableGutters
-			// @ts-expect-error REASON: we get ts error but react native ignores hover related styles
-			style={styles}>
-			<TouchableRipple
-				accessibilityRole='button'
-				{...props.pressableProps}
-				onPress={(event: GestureResponderEvent) => {
-					if (props.onPress) props.onPress(event);
-				}}
-				style={generateStyles}
-				disabled={props.disabled}
-				dataSet={props.dataSets?.containerDataSet}>
-				<>
-					{getIcon()}
-					{props.isLoading ? (
-						<ActivityIndicator color={styles.color as string} />
-					) : null}
-					<Typography.Label
-						dataSet={props.dataSets?.labelDataSet}
-						selectable={false}
-						size='large'
-						style={getCombinedStylesForText(labelStyles, props.labelStyle)}>
-						{props.title}
-					</Typography.Label>
-				</>
-			</TouchableRipple>
-		</Container>
-	);
+  return (
+    <Container
+      dataSet={props.dataSets?.containerDataSet}
+      disableGutters
+      // @ts-expect-error REASON: we get ts error but react native ignores hover related styles
+      style={styles}
+    >
+      <TouchableRipple
+        accessibilityRole='button'
+        {...props.pressableProps}
+        onPress={(event: GestureResponderEvent) => {
+          if (props.onPress) props.onPress(event);
+        }}
+        style={generateStyles}
+        disabled={props.disabled}
+        dataSet={props.dataSets?.containerDataSet}
+      >
+        <>
+          {getIcon()}
+          {props.isLoading ? <ActivityIndicator style={activityIndicatorStyle} color={styles.color as string} /> : null}
+          <Typography.Label
+            dataSet={props.dataSets?.labelDataSet}
+            selectable={false}
+            size='large'
+            style={getCombinedStylesForText(labelStyles, props.labelStyle)}
+          >
+            {props.title}
+          </Typography.Label>
+        </>
+      </TouchableRipple>
+    </Container>
+  );
 };
