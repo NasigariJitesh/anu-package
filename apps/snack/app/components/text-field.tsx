0a77d610f4e015b5905634e23b86be4fd9f72e57
NasigariJitesh <72294952+NasigariJitesh@users.noreply.github.com>
2023-06-19 14:21:20 +0530

    added additional examples for text field, time picker, touchable ripple, typography

--- apps/snack/app/components/text-field.tsx
+++ apps/snack/app/components/text-field.tsx
@@ -1,12 +1,34 @@
 /* eslint-disable react-native/no-inline-styles */
-import { Container, TextField } from 'anu/lib';
+import { Container, Icon, TextField, Typography } from 'anu/lib';
 import React, { useState } from 'react';
 
 const TextFieldScreen = () => {
   const [text, setText] = useState('');
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
   return (
-    <Container style={{ padding: 10 }}>
-      <TextField value={text} onChangeText={setText} />
+    <Container style={{ flex: 1, width: '100%' }}>
+      <Container style={{ padding: 10, width: '100%', height: 150 }}>
+        <Typography.Title style={{ marginBottom: 5 }}>Outlined Text Field</Typography.Title>
+
+        <TextField value={text} onChangeText={setText} />
+      </Container>
+      <Container style={{ padding: 10, width: '100%', height: 150 }}>
+        <Typography.Title style={{ marginBottom: 5 }}>Filled Text Field</Typography.Title>
+
+        <TextField value={text1} onChangeText={setText1} variant='filled' />
+      </Container>
+      <Container style={{ padding: 10, width: '100%', height: 150 }}>
+        <Typography.Title style={{ marginBottom: 5 }}>Text Field - with Icons</Typography.Title>
+
+        <TextField
+          value={text2}
+          onChangeText={setText2}
+          variant='outlined'
+          trailingIcon={<Icon name={'keyboard-tab'} />}
+          leadingIcon={<Icon name={'language'} />}
+        />
+      </Container>
     </Container>
   );
 };
--- apps/snack/app/components/time-picker.tsx
+++ apps/snack/app/components/time-picker.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import { Button, Container, TimePickerModal } from 'anu/lib';
+import { Button, Container, TimePickerModal, Typography } from 'anu/lib';
 import React, { useState } from 'react';
 
 const TimePickerScreen = () => {
@@ -7,22 +7,49 @@ const TimePickerScreen = () => {
   const [minutes, setMinutes] = useState<number>();
   const [hours, setHours] = useState<number>();
 
+  const [visible1, setVisible1] = useState(false);
+  const [minutes1, setMinutes1] = useState<number>();
+  const [hours1, setHours1] = useState<number>();
+
   return (
-    <Container disableGutters style={{ flex: 1 }}>
-      <Button.Outlined onPress={() => setVisible(true)} title='Select time' />
-      <TimePickerModal
-        visible={visible}
-        hours={hours}
-        minutes={minutes}
-        onConfirm={(hoursAndMinutes: { hours: number; minutes: number }) => {
-          setHours(hoursAndMinutes.hours);
-          setMinutes(hoursAndMinutes.minutes);
-          setVisible(false);
-        }}
-        onDismiss={() => {
-          setVisible(false);
-        }}
-      />
+    <Container style={{ flex: 1 }}>
+      <Container style={{ padding: 10 }}>
+        <Typography.Title style={{ marginBottom: 5 }}>Time Picker </Typography.Title>
+
+        <Button.Outlined onPress={() => setVisible(true)} title='Select time' />
+        <TimePickerModal
+          visible={visible}
+          hours={hours}
+          minutes={minutes}
+          onConfirm={(hoursAndMinutes: { hours: number; minutes: number }) => {
+            setHours(hoursAndMinutes.hours);
+            setMinutes(hoursAndMinutes.minutes);
+            setVisible(false);
+          }}
+          onDismiss={() => {
+            setVisible(false);
+          }}
+        />
+      </Container>
+      <Container style={{ padding: 10 }}>
+        <Typography.Title style={{ marginBottom: 5 }}>Time Picker - 24 Hr</Typography.Title>
+
+        <Button.Outlined onPress={() => setVisible1(true)} title='Select time' />
+        <TimePickerModal
+          visible={visible1}
+          hours={hours1}
+          minutes={minutes1}
+          onConfirm={(hoursAndMinutes: { hours: number; minutes: number }) => {
+            setHours1(hoursAndMinutes.hours);
+            setMinutes1(hoursAndMinutes.minutes);
+            setVisible1(false);
+          }}
+          onDismiss={() => {
+            setVisible1(false);
+          }}
+          use24HourClock
+        />
+      </Container>
     </Container>
   );
 };
